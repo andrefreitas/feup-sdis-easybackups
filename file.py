5e6ef9f29a89ce6346ba28d9a3491c2fde94df61
André Freitas <p.andrefreitas@gmail.com>
2013-03-14 17:19:15 +0000

    Implementada a class File. Já consegue obter o nome do ficheiro e calcular o file_id pelo sha256.

---
+++ file.py
@@ -0,0 +1,51 @@
+import re
+import hashlib
+import os
+import datetime
+
+
+class InvalidPathError:
+	pass
+
+class File:
+	def __init__(self, full_path):
+		self.set_full_path(full_path)
+		self.parse_name()
+		self.generate_file_id()
+
+	def parse_name(self):
+		file_extension_pattern="[a-zA-Z0-9_\-]+\.[a-zA-Z0-9]+$"
+		full_path=self.get_full_path()
+		try:
+			self.set_name(re.search(file_extension_pattern,full_path).group(0))
+		except:
+			raise InvalidPathError 
+		
+	def get_modification_date(self):
+		full_path=self.get_full_path()
+		t = os.path.getmtime(full_path)
+		return str(datetime.datetime.fromtimestamp(t))
+	
+	def generate_file_id(self):
+		modification_date=self.get_modification_date()
+		full_path=self.get_full_path()
+		file_id=hashlib.sha256(full_path+modification_date).hexdigest()
+		self.set_file_id(file_id)
+	
+	def set_full_path(self,full_path):
+		self._full_path=full_path
+
+	def get_full_path(self):
+		return self._full_path
+
+	def set_name(self,name):
+		self._name=name
+
+	def get_name(self):
+		return self._name
+
+	def set_file_id(self,file_id):
+		self._file_id=file_id
+
+	def get_file_id(self):
+		return self._file_id
