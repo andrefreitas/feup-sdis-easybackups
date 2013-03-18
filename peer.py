74803d1de9c897188e7ee2f81dffe81764e37043
André Freitas <p.andrefreitas@gmail.com>
2013-03-18 22:59:59 +0000

    Colocadas mensagens de controlo do servidor e apresenta o PID com que é criada a thread principal do peer.py.

--- peer.py
+++ peer.py
@@ -6,15 +6,18 @@ that were generated to send to another peer in the network.
 import os
 import socket
 import struct
-import re
 from file import File
 from threading import Thread
+from datetime import datetime
 
 BACKUP_DIR="backup"
 TEMP_DIR="temp"
 SHELL_PORT=8383
 VERSION="1.0"
 CRLF="\r\n"
+
+def print_message(message):
+    print  "[" + datetime.now().strftime("%d/%m/%y %H:%M") + "] " + message
 
 class Peer:
     def __init__(self, home_dir,mc_address,mc_port,mdb_address,mdb_port,mdr_address,mdr_port,shell_port=SHELL_PORT):
@@ -29,7 +32,8 @@ class Peer:
         self.mdr_port=mdr_port
         self.mdb=self.create_multicast_socket(mdb_address, mdb_port)
         self.mdr=self.create_multicast_socket(mdr_address, mdr_port)
-        self.shell=self.create_socket(shell_port)
+        self.shell_port=shell_port
+        self.shell=self.create_socket(self.shell_port)
         
         
     def init_home_dir(self):
@@ -44,23 +48,30 @@ class Peer:
     
     def listen_shell(self):    
         while True:
-            message = self.shell.recvfrom(8192)[0]      
+            message = self.shell.recvfrom(8192)[0]  
+            print_message("Shell received \"" + message[:len(message)-1]+"\"")    
             self.handle_shell_request(message)
 
     
     def listen_all(self):
+        print_message("Starting EasyBackup Peer Daemon with PID " + str(os.getpid()) + " ...")
         shell = Thread(target=self.listen_shell, args=())
-        mc = Thread(target=self.listen, args=(self.mc,))
-        mdb = Thread(target=self.listen, args=(self.mdb,))
-        mdr = Thread(target=self.listen, args=(self.mdr,))
+        mc = Thread(target=self.listen, args=(self.mc,"MC"))
+        mdb = Thread(target=self.listen, args=(self.mdb,"MDB"))
+        mdr = Thread(target=self.listen, args=(self.mdr,"MDR"))
         shell.start()
-        mdb.start()       
+        print_message("Shell listening at UDP port " + str(self.shell_port))
+        mdb.start()  
+        print_message("MDB listening at Multicast group "+ self.mdb_address + " and port " + str(self.mdb_port))     
         mc.start()
+        print_message("MC listening at Multicast group "+ self.mc_address + " and port " + str(self.mc_port))
         mdr.start()
+        print_message("MDR listening at Multicast group "+ self.mdr_address + " and port " + str(self.mdr_port))
         shell.join()
         mdb.join()
         mc.join()
         mdr.join()
+
     
     def create_multicast_socket(self,multicast_address,multicast_port):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
@@ -76,18 +87,19 @@ class Peer:
         s.bind(("127.0.0.1", port))
         return  s
     
-    def listen(self,sock):
+    def listen(self,sock,channel):
         while True:
-            message = sock.recv(10240)
+            message,addr = sock.recvfrom(10240)
+            operation=message.split(" ")[0]
+            print_message(channel+" received " + operation + " from " + str(addr) )    
             request = Thread(target=self.handle_request, args=(message,))
             request.start()
             request.join()
             
     def handle_request(self, message):
-        print message
+        a=""
        
     def handle_shell_request(self, message):
-        print message
         args=message.split(" ")
         operation=args[0]
         if(operation=="backup"):
