a8528766f9750a270308add0807889e534c53b92
Ana Gomes <acgg.gomes@gmail.com>
2013-03-19 17:56:56 +0000

    Criada a interface da shell para fazer backup dos ficheiros.

---
+++ easybackups.py
@@ -0,0 +1,22 @@
+from shell import Shell
+
+print "Welcome to Easy Backup Shell!"
+print "Type \"help\" to see available commands"
+shell=Shell()
+
+# Read commands
+while(True):
+    command=raw_input("> ").split(" ")
+    operation=command[0]
+    if (operation == "help"):
+        print "    - backup <file> (<replication degree>)"
+        print "    - exit"
+    elif (operation=="backup"):
+        file_name=command[1]
+        replication_degree=2
+        if (len(command)==3):
+            replication_degree=int(command[2])
+        shell.backup_file(file_name, replication_degree)
+    elif(operation=="exit"):
+        break
+        
