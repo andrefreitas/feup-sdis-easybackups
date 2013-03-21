fbd1e9dec00c3601239be3b73691d124fbcf5e57
André Freitas <p.andrefreitas@gmail.com>
2013-03-21 17:58:15 +0000

    Dado que é necessário saber o replication degree de cada chunk, foi adicionada uma tabela na base de dados com o nome chunks.

--- data_setup.sql
+++ data_setup.sql
@@ -1,22 +1,27 @@
 
 PRAGMA foreign_keys = ON;
 
-/* The files tables describes all the files by their fullpath.*/
 DROP TABLE IF EXISTS files;
 CREATE TABLE files(
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	name VARCHAR(30) UNIQUE NOT NULL
 );
 
-/* A certain file can be modified several times so, for a given 
-record from files, there are a lot records in files_modified.
-*/
 DROP TABLE IF EXISTS modifications;
 CREATE TABLE modifications(
-	id VARCHAR(300) PRIMARY KEY NOT NULL, /* SHA256 */
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	sha256 VARCHAR(300) NOT NULL,
 	date_modified DATE NOT NULL,
 	chunks INTEGER NOT NULL,
 	file_id INTEGER NOT NULL REFERENCES files(id)
+);
+
+DROP TABLE IF EXISTS chunks;
+CREATE TABLE chunks(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	number INTEGER NOT NULL,
+	replication_degree INTEGER NOT NULL,
+	modification_id INTEGER NOT NULL REFERENCES modifications(id)
 );
 
 DROP TRIGGER IF EXISTS drop_modifications;
@@ -24,4 +29,11 @@ CREATE TRIGGER drop_modifications
 AFTER DELETE ON files
 BEGIN
 	DELETE FROM modifications WHERE file_id=OLD.id;
+END;
+
+DROP TRIGGER IF EXISTS drop_chunks;
+CREATE TRIGGER drop_chunks
+AFTER DELETE ON modifications
+BEGIN
+	DELETE FROM chunks WHERE modification_id=OLD.id;
 END;
